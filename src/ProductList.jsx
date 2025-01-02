b7d83e25b27552e616d86effaad8162596e980c4
amethystdesign <amethystdesignca@hotmail.com>
2025-01-02 10:29:26 -0500

    add item description

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -288,6 +288,7 @@ const handlePlantsClick = (e) => {
                     <img className="product-image" src={plant.image} alt={plant.name} />
                     <div className="product-title">{plant.name}</div>
                     {/*Similarly like the above plant.name show other details like description and cost*/}
+                    <div className="product-description">{plant.description}</div>
                     <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                   </div>
                 ))}
