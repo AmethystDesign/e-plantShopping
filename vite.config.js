0211788f76b6fcce31fbe67523839bef2fa88427
amethystdesign <amethystdesignca@hotmail.com>
2025-04-20 22:32:01 -0400

    update config  for deploy on Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: "/shoppingreact",
-  base: "/e-plantShopping",
+  // base: "/e-plantShopping",  2025-04-20 commented for testing deploy on Vercel. uncomment it if deploy on gh-pages
   plugins: [react()],
 })
